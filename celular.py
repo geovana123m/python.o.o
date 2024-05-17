ba144afb27169c141bfcb6ada90aa8f493622bf5
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-05-17 11:46:21 -0300

    Add files via upload

---
+++ celular.py
@@ -0,0 +1,20 @@
+class Celular():
+    bateria = 'infinita'
+    tela = '4x3'
+    tem_camera = True
+    tem_botao = True
+    tem_antena = True
+    cor = 'cinza'
+    modelo = 'tijolao'
+
+    def ligacao():
+        print('ligndo...')
+
+    def mensagem():
+        print('enviando mensagem...')
+        
+    def jogo_cobrinha():
+        print('jogando...')
+
+print(Celular.bateria)
+print(Celular.jogo_cobrinha())
